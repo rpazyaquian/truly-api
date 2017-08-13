7be42fb50b2e8a7afc1b659c908363bbc2ce7d03
Rebecca Paz <r.pazyaquian@gmail.com>
2017-08-13 14:16:11 -0400

    handle number lookup, develop data structure

--- src/truly_api/handler.clj
+++ src/truly_api/handler.clj
@@ -12,11 +12,29 @@
 ; {:numbers {"+11111111111" {"context whee" "Rebecca"
 ;                            "Other Context Whee" "whee"}}}
 
-(defn add-number [db entry]
-  ; (println entry)
-  (swap! db update-in [:numbers] entry))
+; i could do some checks before inserting a new piece of data to ensure
+; <number, context> uniqueness, but is that efficient?
 
-(defn get-number [db number])
+; (swap! db update-in [:numbers] entry)
+
+(defn update-numbers [db entry]
+  (let [[number context name] entry]
+    (swap! db assoc-in [number context] name)))
+
+(defn number-context-exists? [db entry]
+  (let [[number context name] entry]
+    ;; NOTE: what to do if "name" is set to nil in the db?
+    (if (not (= nil (get-in @db [number context])))  ;; if number-context exists already and is not nil
+      true
+      false)))
+
+(defn add-number [db entry]
+  (if (number-context-exists? db entry)
+    :error
+    (update-numbers db entry)))
+
+(defn get-number [db number]
+  (get-in @db [number]))
 
 (defn only-digits [number]
   (clojure.string/replace number #"\D" ""))
@@ -48,7 +66,7 @@
 
 (defroutes app-routes
   (GET "/query" [number] (get-number db number))
-  (POST "/number" [data] (add-number db data))
+  (POST "/number" [data] (if (= :error (add-number db data)) 403 "yay"))
   (route/not-found "Hello World"))
 
 (def app
